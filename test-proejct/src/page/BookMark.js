c87a832fbf5f8e469377c35aebc4d2bca753505a
hyeong-il <coding0316@naver.com>
2023-05-16 00:01:51 +0900

    헤더부분변경

---
+++ test-proejct/src/page/BookMark.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function BookMark() {
+    return (
+        <div>
+            <Header/>
+            북마크 페이지
+        </div>
+    );
+}
---
+++ test-proejct/src/page/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header'
+
+export default function Main() {
+    return (
+        <div>
+            <Header/>
+            메인페이지입니다.
+        </div>
+    );
+}
