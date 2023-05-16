cdcdcea8fc39d4fcbff3c668727c9614626eb46f
hyeong-il <coding0316@naver.com>
2023-05-16 11:00:31 +0900

    Footer 기능구현

---
+++ test-proejct/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            <div>개인정보 처리방침 | 이용 약관</div>
+            <div>All rights reserved @ Codestates</div>
+        </footer>
+    );
+};
+
+export default Footer;
--- test-proejct/src/page/ProductList.js
+++ test-proejct/src/page/ProductList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function ProductList() {
     return(
         <div>
             <Header/>
-            프로덕트 리스트 페이지
+            <div className='main'>상품 페이지 입니다</div>
+            <Footer/>
         </div>
     );
 }
